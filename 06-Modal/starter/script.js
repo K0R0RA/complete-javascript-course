a731fb83bd81f034b986c8e3a1a34dacaed02bdf
K0R0RA <81938154+K0R0RA@users.noreply.github.com>
2024-08-28 11:31:09 -0400

    completed modal popup and closure project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const button_els = document.querySelectorAll('.show-modal');
+const modal_el = document.querySelector('.modal');
+const close_el = document.querySelector('.close-modal');
+
+button_els.forEach((button) => {
+  button.addEventListener('click',function(e) {
+    //select the popup div
+    modal_el.classList.remove('hidden'); 
+  });
+});
+
+close_el.addEventListener('click',function(e) {
+  modal_el.classList.add('hidden');
+});
+
+window.addEventListener('keydown',function (event) {
+  if(event.key === 'Escape') {
+    modal_el.classList.add('hidden');
+  }
+});
