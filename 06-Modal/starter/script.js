2926989a434958145a49a6406bb304f9968b6731
K0R0RA <81938154+K0R0RA@users.noreply.github.com>
2024-08-28 12:31:16 -0400

    finished modal popup

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,20 +3,26 @@
 const button_els = document.querySelectorAll('.show-modal');
 const modal_el = document.querySelector('.modal');
 const close_el = document.querySelector('.close-modal');
+const overlay_el = document.querySelector('.overlay');
+
+const closeModal = function() {
+  modal_el.classList.add('hidden');
+  overlay_el.classList.add('hidden');
+};
 
 button_els.forEach((button) => {
-  button.addEventListener('click',function(e) {
+  button.addEventListener('click',function() {
     //select the popup div
     modal_el.classList.remove('hidden'); 
+    overlay_el.classList.remove('hidden');
   });
 });
 
-close_el.addEventListener('click',function(e) {
-  modal_el.classList.add('hidden');
-});
+close_el.addEventListener('click',closeModal);
+overlay_el.addEventListener('click',closeModal);
 
 window.addEventListener('keydown',function (event) {
-  if(event.key === 'Escape') {
-    modal_el.classList.add('hidden');
+  if(event.key === 'Escape' && !modal_el.classList.contains('hidden')) {
+    closeModal();
   }
 });
