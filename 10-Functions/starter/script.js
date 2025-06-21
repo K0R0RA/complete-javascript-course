4c5a8d5a5965f37f671064d7616929fc2c0f9c9c
K0R0RA <81938154+K0R0RA@users.noreply.github.com>
2025-06-21 18:24:07 +0000

    video 135

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+//============= Work Along ========================================================================
+// Video #135
+const booking = [];
+const createBooking = function(flightNum, numPassengers, price) {
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+    console.log(booking);
+    booking.push(booking);
+}
+
+
+//============= Challenges ========================================================================
